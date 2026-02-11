60bc1829047a73a795172f00021c512a79c3998b
aarib444 <aaribu@gmail.com>
2026-02-10 18:49:25 -0700

    finished lab demo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,7 @@ android {
         version = release(36)
     }
 
+
     defaultConfig {
         applicationId = "com.example.listycity"
         minSdk = 24
@@ -31,13 +32,20 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+
+
 }
+
+
 
 dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.0.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.0.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
